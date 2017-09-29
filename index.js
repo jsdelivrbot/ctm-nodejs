4c8c66725b806bca7f1463b25dcd9efc9cd510d4
tnaughton <me@travisnaughton.com.au>
2017-09-29 10:27:02 +1000

    initial upload

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var https = require('https');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+	
+	//response.send(Buffer('eraj.saliev@comparethemarket.com.au:PFs2rNZlQtq0QIyBpt1gwE4UmZjON5A8', 'binary').toString('base64'))
+	console.log('this prints to server log not browser');
+	
+	
+	var postData = 'node3 no length';
+	
+	var options = {
+		'hostname': 'requestb.in',
+		'path': '/17rbpec1',
+		'method': 'POST',
+		'headers': {
+			'Content-Type': 'text/plain'
+		},
+	};
+	
+	var api = https.request(options, function(response) {
+		console.log(response);
+	});
+	
+	api.on('error', function(e) {
+		console.error(e);
+	});
+	
+	api.write(postData);
+	api.end();
+	
+	response.send('CTM Node Application');
+	response.end();
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
